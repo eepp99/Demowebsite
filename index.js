eece466d0f05a4ab89a82d3ff2d9d4f44b782321
eepp99 <ezennaeronini@gmail.com>
2025-06-24 12:18:50 +0100

    index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/data', express.static(path.join(__dirname, '../data')));
-app get
+
 
 // Ensure folders exist
 ['../uploads/items', '../uploads/slideshow', '../data'].forEach(folder => {
