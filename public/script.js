167b42ffefcda52df0ba314b3b51c28c44651f81
eepp99 <ezennaeronini@gmail.com>
2025-06-27 09:57:50 +0100

    script.js

--- public/script.js
+++ public/script.js
@@ -437,7 +437,7 @@ function loadBuyers() {
 function deleteBuyer(index) {
   if (!confirm("Are you sure you want to delete this buyer?")) return;
 
-  fetch(`${API}/api/delete-buyer`, {
+  fetch(`${API}/api/delete-buyer/${index}`, {
     method: "DELETE"
   }).then(() => loadBuyers());
 }
